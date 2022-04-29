8974ca0d6f3790762823407b81ac600624c9a9fe
somaya.tanjina <you@example.com>
2022-04-29 22:53:09 +0600

    single product and its detail is get from mongodb

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-// middleware
+// middleware to connect localhost 3000 & localhost 5000
 app.use(cors());
+
+// middleware to parse json
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oqcrh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -23,13 +25,21 @@ async function run() {
       console.log("Mongodb  added");
 
 // get data from database step-1
+
       app.get('/products', async(req, res) => {
         const query = {};
         const cursor = productCollection.find(query);
         const products = await cursor.toArray()
         res.send(products)
-})
+      })
 
+//get single data by id
+      app.get('/inventory/:id', async(req, res) => {
+        const id = req.params.id;
+        const querry = { _id: ObjectId(id) }
+        const product = await productCollection.findOne(querry)
+        res.send(product)
+      })
 
     } finally {
         // await client.close();
